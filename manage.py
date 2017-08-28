7e9620dc8dc3c211baa4d2b95d0567cebb6d5f89
eric-elem <eric.elem@andela.com>
2017-08-28 13:17:16 +0300

    Update manage

--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 from flask_script import Manager
-from app import create_app
+from app import create_app, db
 from flask_migrate import MigrateCommand
 
 APP = create_app('DEVELOPMENT')
 MANAGER = Manager(APP)
 MANAGER.add_command('db', MigrateCommand)
 
+@manager.command
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     MANAGER.run()
